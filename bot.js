02c44ac3e2405cd647502485e4d3a1fec9aeeaef
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2021-07-18 15:41:36 +0200

    update to kik-api

--- bot.js
+++ bot.js
@@ -13,13 +13,14 @@ const { nextTick } = require("process");
 var count_users=0;
 var count_pictures=0;
 
+var admin_user="";
+var logined_user="";
+
 if (fs.existsSync("sessions")) {
   fs.rmdirSync("sessions", { recursive: true, force: true });
 }
 
 var Kik = new KikClient({
-  username: process.env.KIK_Username,
-  password: process.env.KIK_Passwort,
   promptCaptchas: false,
   trackUserInfo: false,
   trackFriendInfo: false,
@@ -28,42 +29,54 @@ var Kik = new KikClient({
   },
   logger: {
     file: ["warning", "error", "info", "raw"],
-    console: ["error"]
+    console: ["warning", "error", "info"]
   }
 });
 
-Kik.connect();
-//Kik.authenticate(process.env.KIK_Username, process.env.KIK_Passwort)
+Kik.authenticate(process.env.KIK_Username, process.env.KIK_Passwort);
+
 Kik.on("authenticated", () => {
   debug.log("Authenticated", "API");
   
+  //file --mime-type -b 
+  SendImageBack();
+
+  
+  Kik.getUserInfo(process.env.KIK_Username, false, (users) => {
+    logined_user=users[0].jid;
+    debug.log("Set Logined User to: " + logined_user, "APP");
+  });
+
   setTimeout(
     () => {
       Kik.getUserInfo("dickygirl69", false, (users) => {
-        //console.log(users);
-        Kik.sendMessage(users[0].jid,"Status - Users: " + count_users + " Picture: " + count_pictures);
-        //Kik.sendMessage("et51418_yx1@talk.kik.com","Your are welcome!");
+        admin_user=users[0].jid;
+        Kik.sendMessage(admin_user,"Status - Users: " + count_users + " Picture: " + count_pictures);
+        debug.log("Set Admin User to: " + admin_user, "APP");
       });
     }, 10000
   )
-  
 });
 
 Kik.on("receivedgroupmsg", (group, sender, msg) => {
-  debug.log(sender.jid + ": " + msg, "receivedgroupmsg");
+  debug.log(sender + ": " + msg, "receivedgroupmsg");
 });
 Kik.on("receivedgroupimg", (group, sender, img) => {
-  debug.log(sender.jid + " send an Image!", "receivedgroupimg");
-  SendImageBack(sender,Kik);
+  debug.log(sender + " send "+img+"!", "receivedgroupimg");
+  SendImageBack(group,Kik);
 });
 
 Kik.on("receivedprivatemsg", (sender, msg) => {
-  debug.log(sender.jid + ": " + msg, "receivedprivatemsg");
+  debug.log(sender + ": " + msg, "receivedprivatemsg");
 
 });
-Kik.on("receivedprivateimg", (sender, msg, img) => {
-  debug.log(sender.jid + " send an Image!", "receivedprivateimg");
+Kik.on("receivedprivateimg", (sender, img) => {
+  debug.log(sender + " send "+img+"!", "receivedprivateimg");
   SendImageBack(sender, Kik);
+  if (admin_user!=sender) {
+    Kik.sendMessage(admin_user, "Form "+sender+": ");
+    Kik.sendImage(admin_user, __dirname + "/" + img);
+  }
 });
 
 function SendImageBack(sender, client) {
@@ -73,6 +86,9 @@ function SendImageBack(sender, client) {
   var img_folder = path_class.join("images");
   count_users=0;
   count_pictures=0;
+  if (!fs.existsSync(img_folder)) {
+    return;
+  }
   fs.readdirSync(img_folder).forEach(file => {  // istani
     var tmp1 = path_class.join(img_folder ,file);
     if (typeof folder_struct[file] == "undefined") folder_struct[file] = {};
@@ -98,7 +114,7 @@ function SendImageBack(sender, client) {
       for (var u in folder_struct[a][t]) {
         var path3 = path_class.join(path2, u);
         if (typeof sender != "undefined") {
-          if (u != sender.jid) {
+          if (u != sender) {
             for (var f in folder_struct[a][t][u]) {
               pics.push(path_class.join(path3, f));
             }
@@ -116,20 +132,22 @@ function SendImageBack(sender, client) {
   var pic_path = randompic;
   if (typeof sender != "undefined" && typeof pic_path != "undefined") {
     //Kik.sendMessage(sender.jid, "Random Dirty Picture Roulett", (delivered, read) => {});
-    client.sendImage(sender.jid, pic_path, false, false);
-    debug.log("Send " + pic_path + " to " + sender.jid);
+    setTimeout(() => {
+      client.sendImage(sender, pic_path, false, false);
+      debug.log("Send " + pic_path + " to " + sender, "SENDEDPRIVATEIMG");
+    },5000);
   }
-  
 }
 
 const execSync = require('child_process').execSync;
 function CheckImages(pics) {
   for (var idx in pics) {
+    // ToDo: Find a solution for windows!
     var mimeType = execSync('file --mime-type -b "' + pics[idx] + '"').toString();
     var ptype = mimeType.trim();
     switch (ptype) {
       case 'video/mp4':
-        var tmp_file = pics[idx].replace("images", "videos").replace(".jpeg", ".avi");
+        var tmp_file = pics[idx].replace("images", "videos");
         debug.error("Stupid File: " + tmp_file, "USER");
         var tmp_str = tmp_file.split("/", 4);
         var tmp_path = tmp_str.join("/");
@@ -144,6 +162,3 @@ function CheckImages(pics) {
     }
   }
 }
-
-//file --mime-type -b 
-SendImageBack();
